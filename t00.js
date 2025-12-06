4f6f6f4689c477efefab3b7ab22d1168d0c44825
insar89 <insar21@gmail.com>
2025-12-06 17:47:04 +0300

    feat: lesson 3

--- t00.js
+++ t00.js
@@ -1,1 +1,24 @@
-console.log("test");
+const user = {
+    name: 'VasyYa',
+    married: true,
+    age: 25,
+    test: 'БесКонЧЕность и ДаЛЬше',
+
+}
+
+user.married = false;
+user.surname = 'Petrov';
+
+const test = {};
+
+
+const normalize = (lesson) => {
+    for (const el in lesson) {
+        lesson[el] = lesson[el].toString().charAt(0).toUpperCase() + lesson[el].toString().slice(1).toLowerCase();
+    }
+}
+
+normalize(user);
+
+
+  console.log(user);
